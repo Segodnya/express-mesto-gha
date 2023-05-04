8788016c241ab150db9dfaf7a47d7a0534f67c5d
Kirill Kurentsov <immortalsteel@gmail.com>
2023-05-04 06:12:04 +0300

    feat: add url validate from validator

--- models/card.js
+++ models/card.js
@@ -1,30 +1,37 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: [2, 'Минимальная длина поля 2 символа'],
-    maxlength: [30, 'Максимальная длина поля 30 символов'],
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: [2, 'Минимальная длина поля 2 символа'],
+      maxlength: [30, 'Максимальная длина поля 30 символов'],
+    },
+    link: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => validator.isURL(v),
+      },
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'user',
+      default: [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  link: {
-    type: String,
-    required: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  likes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: 'user',
-    default: [],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false },
+);
 
 module.exports = mongoose.model('card', cardSchema);
--- models/user.js
+++ models/user.js
@@ -1,22 +1,29 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: [2, 'Минимальная длина поля 2 символа'],
-    maxlength: [30, 'Максимальная длина поля 30 символов'],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: [2, 'Минимальная длина поля 2 символа'],
+      maxlength: [30, 'Максимальная длина поля 30 символов'],
+    },
+    about: {
+      type: String,
+      required: true,
+      minlength: [2, 'Минимальная длина поля 2 символа'],
+      maxlength: [30, 'Максимальная длина поля 30 символов'],
+    },
+    avatar: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => validator.isURL(v),
+      },
+    },
   },
-  about: {
-    type: String,
-    required: true,
-    minlength: [2, 'Минимальная длина поля 2 символа'],
-    maxlength: [30, 'Максимальная длина поля 30 символов'],
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-});
+  { versionKey: false },
+);
 
 module.exports = mongoose.model('user', userSchema);
