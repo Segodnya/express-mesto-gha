4fe7e4baa9e77ca61f584c9a4ee90d052f21a884
Kirill Kurentsov <immortalsteel@gmail.com>
2023-05-09 22:35:09 +0300

    refactor: replace header-jwt with httpOnly-cookie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,11 +3,10 @@ const UnauthorizedError = require('../utils/errors/unauthorizedError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const token = req.cookies.jwt;
+  if (!token) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
-  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-string');
